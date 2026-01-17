c460fd112931c1054800427a02c1131557a2536b
atalekar-r7 <akhilesh_talekar@rapid7.com>
2026-01-17 21:31:12 +0530

    Fix syntax for variable definitions in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
-    description: "This is the region in which i will create my instance"
-    value: string
+    description = "This is the region in which i will create my instance"
+    value = string
     default = "us-east-1"
 }
 
 variable "instance_type" {
-    description: "This is the region in which i will create my instance"
-    value: string
+    description = "This is the region in which i will create my instance"
+    value = string
     default = "t3.micro"
 }
