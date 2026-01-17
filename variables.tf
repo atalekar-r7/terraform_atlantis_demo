a5f031e6f88c084c5ed8f094182e850141d0da39
atalekar-r7 <akhilesh_talekar@rapid7.com>
2026-01-17 21:20:48 +0530

    Fix default instance type to t2.micro in variables.tf

--- variables.tf
+++ variables.tf
@@ -7,5 +7,5 @@ variable "aws_region" {
 variable "instance_type" {
     description: "This is the region in which i will create my instance"
     value: string
-    default = "t3.micro"
+    default = "t2.micro"
 }
