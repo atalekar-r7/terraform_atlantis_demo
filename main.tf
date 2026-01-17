21695c44f247186d3399afa2cb1fba67f01c60f8
atalekar-r7 <akhilesh_talekar@rapid7.com>
2026-01-17 17:52:53 +0530

    Innitial push to make the files available

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "periperi" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = var.instance_type
+    tags = {
+        name: "atlantis-demo"
+    }
+}
